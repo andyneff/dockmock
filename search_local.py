ac943acbfca04989ac51cd047c4c9bfe457f87ea
Andy Neff <andrew.neff@visionsystemsinc.com>
2016-01-06 14:40:27 -0500

    Various with/without fixes, including fixing #12
    -dockrpm - Fixed with/without args for yum-builddep call
    -dockrpm - Fixed with/without args for rmpbuild call and helpful debug messages
    -dep_check - Fixed with/without args to actually work
    -other - Fixed with/without args to actually work
    -dep_check - Added with/without args to search_local. This fixes #11
    -dep_check - Added provides support to search_local. This fixes #11
    -dockrpm - Added --bash debug support for dep_check image... not fully functional,
    but good enough to support dropping into the container for easier debugging.

--- search_local.py
+++ search_local.py
@@ -5,6 +5,7 @@ import os
 from subprocess import Popen, PIPE
 import shutil
 from distutils.dir_util import mkpath
+import argparse
 
 def test_version(version1, version2, test):
   try:
@@ -28,15 +29,15 @@ def test_version(version1, version2, test):
     raise Exception('Unexpected logic test %s' % test)
 
 
-def get_rpm_names_from_specfile(spec_path, source_dir):
-  cmd = ['rpm', '-q', '-D', '_sourcedir %s' % source_dir, 
-         '--qf', '%{NAME}\t%{VERSION}\n', '--specfile', spec_path]
+def get_rpm_names_from_specfile(spec_path, source_dir, other=[]):
+  cmd = ['rpmspec', '-q', '-D', '_sourcedir %s' % source_dir] + other + \
+        ['--qf', '%{NAME}\t%{VERSION}\n%{PROVIDES}\t%{VERSION}\n', spec_path]
   pid = Popen(cmd, stdout=PIPE, stderr=PIPE)
   stdout = pid.communicate()[0][:-1] #get rid of trailing newline
   pid.wait()
   return map(lambda x:x.split('\t'), stdout.splitlines())
 
-def search_local(package_name, version_test=None, version=None, specs_dir='/specs', package_name_suffix='_local'):
+def search_local(package_name, version_test=None, version=None, specs_dir='/specs', package_name_suffix='_local', other=[]):
   #I think the package_name_suffix feature should be removed now...
   local_pacakge_name = package_name.split('-')
   local_pacakge_names = ['-'.join(local_pacakge_name[0:x] + 
@@ -63,7 +64,7 @@ def search_local(package_name, version_test=None, version=None, specs_dir='/spec
       mkpath(os.path.dirname(common_inc))
       shutil.copy(os.path.join(specs_dir, 'common.inc'), common_inc)
       temp_common_inc = True
-    rpm_names = get_rpm_names_from_specfile(spec, os.path.join(os.path.dirname(spec), 'source'))
+    rpm_names = get_rpm_names_from_specfile(spec, os.path.join(os.path.dirname(spec), 'source'), other=other)
     if temp_common_inc:
       os.remove(common_inc)
     for rpm_name in rpm_names:
@@ -76,4 +77,10 @@ def search_local(package_name, version_test=None, version=None, specs_dir='/spec
             return os.path.relpath(spec, specs_dir)
 
 if __name__=='__main__':
-  print search_local(*sys.argv[1:])
+  parser = argparse.ArgumentParser()
+  parser.add_argument('name')
+  parser.add_argument('-t', default=None)
+  parser.add_argument('-v', default=None)
+  (args, other) = parser.parse_known_args()
+
+  print search_local(args.name, args.t, args.v, other=other)
